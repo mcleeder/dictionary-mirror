97bd2ea04d6d75360b31e6b0914dc5285dfe9734
Mike L <mike.leeder@gmail.com>
2023-11-15 18:32:31 -0800

    move files

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from markupsafe import Markup
+from dictionary import dictionary
+import json
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello_world(world: str = "world"):
+    return Markup("<p>Hello, %s!</p>") % world.title()
+
+@app.route("/dictionary/<string:topic>", methods=['GET'])
+def topics(topic: str = None):
+    return json.dumps(dictionary.lookup(topic))
---
+++ dictionary.py
@@ -0,0 +1,41 @@
+import requests
+from lxml import html
+from scraper import element, elements
+
+class Dictionary():
+    def _get_raw_word(self, word: str) -> requests.Response:
+        response = requests.get(f"https://www.dictionary.com/browse/{word}")
+        return response.content
+    
+    def _parse_word(self, raw_html: str) -> dict:
+        html_element = html.fromstring(raw_html)
+
+        if definition_card := element(html_element, "//section[@data-type='word-definition-card']"):
+            word = element(definition_card, "//h1").text
+            pro = element(definition_card, "//span[@data-type='pronunciation-text']").text
+
+            definition_elements = elements(definition_card, "div[@data-type='word-definitions']")
+            definitions = []
+            for elem in definition_elements:
+                word_type = element(elem, "span/span[@class='luna-pos']").text
+                word_defs = []
+                for li in elements(elem, "div/ol/li"):
+                    word_defs.append(element(li, "div/p").text)
+                definitions.append({
+                    "word_type" : word_type,
+                    "word_definitions" : word_defs #[x for x in word_defs if x],
+                })
+                
+
+            return {
+                "word" : word,
+                "pronunciation" : pro[1:-1].strip(), # [ bahrn  ]
+                "definitions" : definitions,
+            }
+        return {"word" : "Oops, something went wrong"}
+    
+    def lookup(self, word: str) -> dict:
+        raw_data = self._get_raw_word(word)
+        return self._parse_word(raw_data)
+    
+dictionary = Dictionary()
